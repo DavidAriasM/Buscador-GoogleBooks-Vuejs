5b3c2fdb2d4e4829d78f47483008c68c34ff3d07
“David” <arias.maulen@gmail.com>
2022-04-07 23:56:15 -0400

    Eliminación de dato de prueba de buscador

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,7 +31,7 @@ export default {
   data() {
     return {
       libros: [],
-      busqueda: 'Sebastian Fitzek',
+      busqueda: '',
       criterio: '',
       estadoCheck: false,
       criterioBusqueda: 'intitle',
